953f6e78bd11d997bbe0bf324ded1dfd87ab70a2
ritika1993 <saoritika@gmail.com>
2019-03-19 01:55:55 +0000

    Update ExceptionResponse.java

--- URL_Shortening_Rest_API/src/main/java/com/sample/url/exception/ExceptionResponse.java
+++ URL_Shortening_Rest_API/src/main/java/com/sample/url/exception/ExceptionResponse.java
@@ -2,7 +2,7 @@ package com.sample.url.exception;
 
 import java.time.LocalDateTime;
 /***
- * DTO for generating an Exception Response for all exceptions handled at URLResponseEntityExceptionHandler
+ * This class is an Entity for generating an Exception Response for all exceptions handled at URLResponseEntityExceptionHandler
  * @author Ritika Sao
  *
  */
@@ -13,31 +13,23 @@ public class ExceptionResponse {
 	// Details of Response
 	private String details;
 	// Timestamp of Response
-	private LocalDateTime timeStamp;
-	
+	private LocalDateTime timeStamp;	
 	
 	public ExceptionResponse(String requestInfo, String details, LocalDateTime timeStamp) {
-		super();
 		this.requestInfo = requestInfo;
 		this.details = details;
 		this.timeStamp = timeStamp;
 	}
 
-
 	public String getRequestInfo() {
 		return requestInfo;
 	}
-
 
 	public String getDetails() {
 		return details;
 	}
 
-
 	public LocalDateTime getTimeStamp() {
 		return timeStamp;
-	}
-	
-	
-	
+	}	
 }
